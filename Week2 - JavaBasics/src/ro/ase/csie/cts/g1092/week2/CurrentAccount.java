af82b8c8e0ac0b60e4fe003021b35009e75d6bc6
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-03-02 11:28:40 +0200

    Finished week 2 seminar

---
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/CurrentAccount.java
@@ -0,0 +1,38 @@
+package ro.ase.csie.cts.g1092.week2;
+
+import ro.ase.csie.cts.g1092.week2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1092.week2.exceptions.InsufficientFundsException;
+
+public class CurrentAccount extends BankAccount {
+
+	public static final double MAX_CREDIT = 5000;
+	
+	public CurrentAccount(String IBAN) {
+		super(CurrentAccount.MAX_CREDIT, IBAN);
+	}
+
+	@Override
+	public void deposit(double amount) {
+		this.balance += amount;
+	}
+
+	@Override
+	public void transfer(Account destination, double amount) 
+			throws InsufficientFundsException, 
+			IllegalTransferException {
+		
+		if(this == destination)
+			throw new IllegalTransferException("Transfer cannot be made to same account.");
+		this.withdraw(amount);
+		destination.deposit(amount);
+	}
+
+	@Override
+	public void withdraw(double amount) throws InsufficientFundsException {
+		if(amount > this.balance)
+			throw new InsufficientFundsException("You do not have the available amount.");
+		else
+			this.balance -= amount;
+	}
+
+}
