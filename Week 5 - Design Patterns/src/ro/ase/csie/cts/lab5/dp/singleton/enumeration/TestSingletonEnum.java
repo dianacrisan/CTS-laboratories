b8a963274350e06f7229a9f132969b096df29d83
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-03-23 11:21:42 +0200

    Singleton enum

---
+++ Week 5 - Design Patterns/src/ro/ase/csie/cts/lab5/dp/singleton/enumeration/TestSingletonEnum.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.lab5.dp.singleton.enumeration;
+
+public class TestSingletonEnum {
+
+	public static void main(String[] args) {
+		DBConnection connection = DBConnection.INSTANCE;
+
+	}
+
+}
