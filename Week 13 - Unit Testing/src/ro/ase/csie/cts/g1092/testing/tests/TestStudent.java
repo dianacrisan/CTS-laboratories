b1327c6db2c88141b70acebf410e717a5bbef601
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-05-25 11:27:43 +0300

    Implementing other unit tests

--- Week 13 - Unit Testing/src/ro/ase/csie/cts/g1092/testing/tests/TestStudent.java
+++ Week 13 - Unit Testing/src/ro/ase/csie/cts/g1092/testing/tests/TestStudent.java
@@ -1,10 +1,13 @@
 package ro.ase.csie.cts.g1092.testing.tests;
 
 import ro.ase.csie.cts.g1092.testing.exceptions.WrongAgeException;
+import ro.ase.csie.cts.g1092.testing.exceptions.WrongGradesException;
 import ro.ase.csie.cts.g1092.testing.exceptions.WrongNameException;
 import ro.ase.csie.cts.g1092.testing.models.Student;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -78,7 +81,70 @@ public class TestStudent {
 		} catch (WrongAgeException e) {
 			fail("We got an exception when that was not expected");
 		}
-    	
-    	
     }
+    
+    @Test
+    public void testSetNameErrorCondition() {
+    	String newName = "Io";
+    	try {
+			student.setName(newName);
+			fail("We didn't get the expected exception");
+		} catch (WrongNameException e) {
+			assertTrue(true);
+		}
+    }
+    
+    // equivalent of the previous one
+    @Test(expected = WrongAgeException.class)
+    public void testSetAgeErrorCondition() throws WrongAgeException {
+    	int newAge = -4;
+    	student.setAge(newAge);
+    }
+    
+    @Test
+    public void testGetMinGradeOrderingAsc() throws WrongGradesException {
+    	ArrayList<Integer> orederedGrades = new ArrayList<>();
+    	orederedGrades.add(7);   	
+    	orederedGrades.add(8);   	
+    	orederedGrades.add(9);   	
+    	
+    	student.setGrades(orederedGrades);
+    	
+    	int expectedGrade = 7;
+    	int minGrade = student.getMinGrade();
+    	
+    	assertEquals("Testing with an ordered array of grades",  expectedGrade, minGrade);
+    }
+    
+	@Test // cardinality zero - you test with 0 values in the collection
+	public void testGetMinGradeCardinalityZero() throws WrongGradesException {
+		ArrayList<Integer> emptyGrades = new ArrayList<>();
+		student.setGrades(emptyGrades);
+		
+		int expectedGrade = 0;
+		int minGrade = student.getMinGrade();
+		
+		assertEquals("Testing with zero grades", expectedGrade, minGrade);
+	}
+	 
+	@Test // cardinality one - you test with 1 value in the collection
+	public void testGetMinGradeCardinalityOne() throws WrongGradesException {
+		int singleGrade = 9;
+		ArrayList<Integer> oneGrade = new ArrayList<>();
+		oneGrade.add(singleGrade);
+		student.setGrades(oneGrade);
+		
+		int expectedGrade = singleGrade;
+		int minGrade = student.getMinGrade();
+		
+		assertEquals("Testing with one grade", expectedGrade, minGrade);
+	}
+	
+	@Test
+	public void testSetNameInverseRelation() throws WrongNameException {
+		String newName = "Alice";
+		student.setName(newName);
+		
+		assertNotEquals(initialName, student.getName());
+	}
 }
